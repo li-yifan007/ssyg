4cdf96d5d3fd5b9ff9afe2181f1e068fbdc7d1a3
李一凡 <11225074+liyifande@user.noreply.gitee.com>
2022-10-20 11:57:40 +0800

    完成了 tabBar 功能的开发

---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		Home666666666666666666666666
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>

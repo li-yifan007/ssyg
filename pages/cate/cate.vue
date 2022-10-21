2ec08afa65b7e9243aa41cfde2ee6f4e1b884a1c
李一凡 <11225074+liyifande@user.noreply.gitee.com>
2022-10-21 16:55:33 +0800

    完成了分类页面的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,31 @@
 <template>
 	<view>
-		
+		<view class="scroll-view-container">
+			<!-- 左侧区域 -->
+			<scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
+				<bolok v-for="(item,i) in cateList" :key="i">
+					<view :class="['left-scroll-view-item',i === active ? 'active' : '']" @click="activeChanged(i)">
+						{{item.cat_name}}</view>
+				</bolok>
+			</scroll-view>
+			<!-- 右侧区域 -->
+			<scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+				<view v-for="(item2,i2) in cateLevel2" :key="i2" class="cate-lv2">
+					<view class="cate-lv2-title">/{{item2.cat_name}}/</view>
+					<view class="cate-lv3-list">
+					      <!-- 三级分类 Item 项 -->
+					      <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
+					        <!-- 图片 -->
+					        <!-- <image :src="item3.cat_icon"></image> -->
+							<image src="http://inews.gtimg.com/newsapp_bt/0/14774249285/641"></image>
+					        <!-- 文本 -->
+					        <text>{{item3.cat_name}}</text>
+					      </view>
+					    </view>
+				</view>
+
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +33,109 @@
 	export default {
 		data() {
 			return {
-				
+				wh: 0,
+				cateList: [],
+				// 当前选中项的索引，默认让第一项被选中
+				active: 0,
+				cateLevel2: [],
+				// 滚动条距离顶部的距离
+				    scrollTop: 0
 			};
+		},
+		onLoad() {
+			const sysInfo = uni.getSystemInfoSync()
+			this.wh = sysInfo.windowHeight
+			this.getCateList()
+		},
+		methods: {
+			async getCateList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/categories')
+				// console.log(res);
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.cateList = res.message
+				// 二级分类赋值
+				this.cateLevel2 = res.message[0].children
+			},
+
+			activeChanged(i) {
+				this.active = i
+				// 重新赋值,使切换后可以更新数据
+				this.cateLevel2 = this.cateList[i].children
+				this.scrollTop = this.scrollTop ? 0 : 1
+			},
+			
+			gotoGoodsList(item3){
+				uni.navigateTo({
+					url:'/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+				})
+			}
+
 		}
 	}
 </script>
 
 <style lang="scss">
+	.scroll-view-container {
+		display: flex;
 
+		.left-scroll-view {
+			width: 120px;
+
+			.left-scroll-view-item {
+				line-height: 60px;
+				background-color: #f7f7f7;
+				text-align: center;
+				font-size: 12px;
+
+				// 激活项的样式
+				&.active {
+					background-color: #ffffff;
+					position: relative;
+
+					// 渲染激活项左侧的红色指示边线
+					&::before {
+						content: ' ';
+						display: block;
+						width: 3px;
+						height: 30px;
+						background-color: #c00000;
+						position: absolute;
+						left: 0;
+						top: 50%;
+						transform: translateY(-50%);
+					}
+				}
+			}
+		}
+	}
+
+	.cate-lv2-title {
+		font-size: 24rpx;
+		font-weight: bold;
+		text-align: center;
+		padding: 30rpx 0;
+	}
+	.cate-lv3-list {
+	  display: flex;
+	  flex-wrap: wrap;
+	
+	  .cate-lv3-item {
+	    width: 33.33%;
+	    margin-bottom: 10px;
+	    display: flex;
+	    flex-direction: column;
+	    align-items: center;
+	
+	    image {
+	      width: 60px;
+	      height: 60px;
+	    }
+	
+	    text {
+	      font-size: 12px;
+	    }
+	  }
+	}
 </style>

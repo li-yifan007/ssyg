dfa59514b46380e2add73ec52bb44e53fc21ecaa
李一凡 <11225074+liyifande@user.noreply.gitee.com>
2022-10-26 17:48:18 +0800

    完成了商品详情页面的开发

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,34 @@
 <template>
-	<view>
-		
+	<view v-if="goods_info.goods_name">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(goods,i) in goods_info.pics" :key="i">
+				<image :src="goods.pics_big" @click="preview(i)"></image>
+			</swiper-item>
+		</swiper>
+
+		<!-- 主体区域 -->
+		<view class="goods-info-box">
+			<!-- 价格 -->
+			<view class="price">
+				￥{{goods_info.goods_price}}
+			</view>
+			<!-- 商品信息 -->
+			<view class="goods-info-body">
+				<view class="goods-name">{{goods_info.goods_name}}</view>
+				<view class="favi">
+					<uni-icons type="star" size="18" color="gray"></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+			<view class="yf">快递：免运费</view>
+		</view>
+
+		<rich-text :nodes="goods_info.goods_introduce"></rich-text>
+
+		<view class="goods_nav">
+			<uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick" @buttonClick="buttonClick" />
+		</view>
+
 	</view>
 </template>
 
@@ -8,12 +36,131 @@
 	export default {
 		data() {
 			return {
-				
+				goods_info: {},
+				options: [{
+					icon: 'shop',
+					text: '店铺'
+				}, {
+					icon: 'cart',
+					text: '购物车',
+					info: 2
+				}],
+				// 右侧按钮组的配置对象
+				buttonGroup: [{
+						text: '加入购物车',
+						backgroundColor: '#ff0000',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: '#ffa200',
+						color: '#fff'
+					}
+				]
 			};
+		},
+		onLoad(options) {
+			// 获取商品 Id
+			const goods_id = options.goods_id
+			// 调用请求商品详情数据的方法
+			this.getGoodsDetail(goods_id)
+		},
+		methods: {
+			// 定义请求商品详情数据的方法
+			async getGoodsDetail(goods_id) {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/detail', {
+					goods_id
+				})
+				if (res.meta.status !== 200) return uni.$showMsg()
+				res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g,
+					'<img style="display:block;" ').replace(/webp/g, 'jpg')
+
+				this.goods_info = res.message
+			},
+			preview(i) {
+				// 调用 uni.previewImage() 方法预览图片
+				uni.previewImage({
+					// 预览时，默认显示图片的索引
+					current: i,
+					// 所有图片 url 地址的数组
+					urls: this.goods_info.pics.map(x => x.pics_big)
+				})
+			},
+			onClick(e){
+				console.log(e);
+				if(e.content.text === '购物车'){
+					uni.switchTab({
+						url:'/pages/cart/cart'
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 750rpx;
 
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.goods-info-box {
+		padding: 10px;
+		padding-right: 0;
+
+		.price {
+			color: #c00000;
+			font-size: 18px;
+			margin: 10px 0;
+		}
+
+		.goods-info-body {
+			display: flex;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+				padding-right: 10px;
+			}
+
+			// 收藏区域
+			.favi {
+				width: 120px;
+				font-size: 12px;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				border-left: 1px solid #efefef;
+				color: gray;
+			}
+		}
+
+		// 运费
+		.yf {
+			margin: 10px 0;
+			font-size: 12px;
+			color: gray;
+		}
+	}
+	
+	.goods-detail-container {
+	  // 给页面外层的容器，添加 50px 的内padding，
+	  // 防止页面内容被底部的商品导航组件遮盖
+	  padding-bottom: 50px;
+	}
+	
+	.goods_nav {
+	  // 为商品导航组件添加固定定位
+	  position: fixed;
+	  bottom: 0;
+	  left: 0;
+	  width: 100%;
+	}
 </style>
